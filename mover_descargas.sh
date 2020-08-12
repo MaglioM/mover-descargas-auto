94b45240ffac632f80512b42465e994da1ae374e
villanos002 <matiashmaglio@gmail.com>
2020-08-12 15:40:49 -0300

    Primer version completamente funcional Soporte de formatos principales Borrado condicional innecesario

--- mover_descargas.sh
+++ mover_descargas.sh
@@ -1,7 +1,28 @@
-#/bin/bash
+#!/bin/bash
 
-for archivo in $(find /home/dinamo/Downloads -name "*.jpg"); do
-	if [ -e "$archivo" ]; then
-		mv $archivo /home/dinamo/Pictures
-	fi
+#IFS: con el valor '\n', reconoce archivos con espacios en el nombre
+IFS=$'\n'
+
+#Imagenes
+for archivo in $(find /home/$USER/Downloads -regextype egrep -regex  ".*(.jpe?g|.png)$");
+do
+		mv $archivo /home/$USER/Pictures
+done
+
+#Videos
+for archivo in $(find /home/$USER/Downloads -regextype egrep -regex  ".*(.mp4|.mov|.avi|.mkv)$");
+do
+		mv $archivo /home/$USER/Videos
+done
+
+#Documentos
+for archivo in $(find /home/$USER/Downloads -regextype egrep -regex  ".*(.pdf|.doc|.txt)");
+do
+		mv $archivo /home/$USER/Documents
+done
+
+#Música
+for archivo in $(find /home/$USER/Downloads -regextype egrep -regex  ".*(.wav|.mp3)");
+do
+		mv $archivo /home/$USER/Music
 done
